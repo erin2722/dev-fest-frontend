ba2358bd19e59a1f431243c5ea9485912127b09d
erin2722 <erin2722@gmail.com>
2020-02-08 18:39:24 -0500

    added form

---
+++ src/components/InputForm.jsx
@@ -0,0 +1,145 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import DateFormat from 'dateformat';
+
+//var dateFormat = require('dateformat');
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 20px;
+  background-color: lightgray;
+  padding: 15px;
+  width: 500px;
+`;
+
+const NameForm = styled.select`
+  cursor: pointer;
+  border-radius: 10px;
+  padding: 5px;
+  background-color: #bbbeff;
+  width: 140px;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  font-size: 14px;
+  :focus {
+    outline: none;
+  }
+`;
+
+const TimeForm = styled.div``;
+
+const Option = styled.option`
+  cursor: pointer;
+  background-color: #bbbeff;
+  border-radius: 10px;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  font-size: 14px;
+`;
+
+const Label = styled.div`
+  margin-bottom: 10px;
+`;
+
+const SubmitButton = styled.button`
+    padding: 10px;
+    margin: 10px;
+    width: 100px;
+`;
+
+class InputForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nameValue: "",
+      timeValue: "",
+      startDate: new Date(),
+      formattedDate: ""
+    };
+
+    console.log(this.props.tasks);
+
+    this.handleTaskChange = this.handleTaskChange.bind(this);
+    this.handleTimeChange = this.handleTimeChange.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.submit = this.submit.bind(this);
+  }
+
+  formatDate(date) {
+    var fDate = DateFormat(this.state.startDate, "isoDate");
+    var fTime = DateFormat(this.state.startDate, "isoTime");
+    fDate = fDate.replace("T", " ");
+    //fDate = fDate.splice(0, 18);
+    return fDate + " " + fTime;
+  }
+
+  handleTaskChange(event) {
+    console.log(event.target.value)
+    this.setState({
+        nameValue: event.target.value
+    })
+  }
+
+  handleTimeChange(event) {
+    console.log(event.target.value);
+    this.setState({
+        timeValue: event.target.value
+    })
+  }
+
+  handleDateChange = date => {
+    this.setState({
+      startDate: date,
+      formattedDate: this.formatDate(date)
+    });
+    console.log(this.formatDate(date));
+  };
+
+  submit() {
+    console.log({name: this.state.nameValue, estimated_duration: this.state.timeValue, due_date: this.state.formattedDate });
+    return {name: this.state.nameValue, estimated_duration: this.state.timeValue, due_date: this.state.formattedDate };
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <Label>Enter a Task:</Label>
+        <NameForm value={this.props.value} onChange={this.handleTaskChange}>
+          <Option value="">Choose an existing Task, or enter in a new one:</Option>
+          <Option name="new" value="new">
+            New Task
+          </Option>
+          {this.props.tasks.map(item => (
+            <Option name={item} id = {item}>
+              {item}
+            </Option>
+          ))}
+        </NameForm>
+        <TimeForm>
+            How much time will this task take? (Enter in Hours)
+            <div><input type = "text" name = "time" onChange = {this.handleTimeChange} /></div>
+        </TimeForm>
+        Select the Time and Date Your Task is due:
+        <DatePicker 
+              placeholderText="Click to select a date"
+              selected={this.state.startDate}
+              onChange={this.handleDateChange}
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              timeCaption="time"
+              dateFormat="yyyy-MM-dd h:mm aa"
+        />
+        <SubmitButton onClick = {this.submit}>Submit</SubmitButton>
+      </Wrapper>
+    );
+  }
+}
+
+export default InputForm;
